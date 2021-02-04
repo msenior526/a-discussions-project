531f82f58a5a2babcef4c9eb999ded3cf5c68db3
msenior526 <msenior526@gmail.com>
2021-02-04 17:01:43 -0500

    create controllers and views for users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
     has_many :comments, through: :posts
     has_secure_password
     validates :username, presence: true, uniqueness: true, length: {in: 6..25}
-    validates :email, presence: true, uniqueness: true, confirmation: true, format: {with:/\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
-    validates :password, length: {in: 8..100}, confirmation: true
+    validates :email, presence: true, uniqueness: true, confirmation: true, format: {with:/\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i, message: "please enter a valid email address"}
+    validates :password, length: {in: 8..100}, confirmation: true #on :create
 end
