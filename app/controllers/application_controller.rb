293d10a4c23aba39cd1193b9480ffce68125d1aa
msenior526 <msenior526@gmail.com>
2021-02-06 11:13:24 -0500

    Add error block to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,22 @@ class ApplicationController < Sinatra::Base
     set :session_secret, ENV['SESSION_SECRET']
   end
 
+  error Sinatra::NotFound do 
+    erb :error
+  end 
+
   get "/" do
     erb :welcome
   end
 
   helpers do
+  	def current_user
+			@current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    
     def logged_in?
-			!!session[:user_id]
+			!!current_user
 		end
-
-		def current_user
-			User.find(session[:user_id])
-		end 
   end
 
 
