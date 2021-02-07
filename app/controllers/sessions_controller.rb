be3f2e4126414dc89cdf3b5b3e32c3052f18c6a5
msenior526 <msenior526@gmail.com>
2021-02-06 20:26:13 -0500

    undo comment on error block

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,6 @@ class SessionsController < ApplicationController
   # POST: /sessions
   post "/login" do
     user = User.find_by_email(params[:user][:email])
-  
-  
     if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect "/users/#{user.id}"
