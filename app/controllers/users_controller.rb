5c24918096afc5a883ca0e2794b02287cf681770
msenior526 <msenior526@gmail.com>
2021-02-07 13:41:21 -0500

    remove unused routes in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-  # # GET: /users
-  # get "/users" do
-  #   erb :"/users/index.html"
-  # end
-
   # GET: /users/new, SIGNUP PAGE
   get "/signup" do
     erb :"/users/signup.html"
@@ -26,16 +21,6 @@ class UsersController < ApplicationController
   get "/users/:id" do
     erb :"/users/show.html"
   end
-
-  # # GET: /users/5/edit
-  # get "/users/:id/edit" do
-  #   erb :"/users/edit.html"
-  # end
-
-  # # PATCH: /users/5
-  # patch "/users/:id" do
-  #   redirect "/users/:id"
-  # end
 
   # # DELETE: /users/5/delete
   # delete "/users/:id/delete" do
