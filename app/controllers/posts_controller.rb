966b000756982882f9f29bd7decd3b185e46d0e2
msenior526 <msenior526@gmail.com>
2021-02-07 13:03:50 -0500

    edit patch route in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,11 +25,16 @@ class PostsController < ApplicationController
 
   # GET: /posts/5/edit
   get "/posts/:id/edit" do
+    @post = Post.find_by_id(params[:id])
     erb :"/posts/edit.html"
   end
 
   # PATCH: /posts/5
   patch "/posts/:id" do
+    @post = Post.find_by_id(params[:id])
+    @post.title = params[:title]
+    @post.content = params[:decription]
+    @post.save
     redirect "/posts/:id"
   end
 
