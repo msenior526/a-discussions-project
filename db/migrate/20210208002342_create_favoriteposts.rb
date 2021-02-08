f5b2be60aead72dcc14ef133efaac60817aeb648
msenior526 <msenior526@gmail.com>
2021-02-07 19:31:30 -0500

    create migration table for favoriteposts

--- db/migrate/20210208002342_create_favoriteposts.rb
+++ db/migrate/20210208002342_create_favoriteposts.rb
@@ -1,7 +1,8 @@
-class CreateFavoriteposts < ActiveRecord::Migration
+class CreateFavoriteposts < ActiveRecord::Migration[5.2]
   def change
     create_table :favoriteposts do |t|
-
+      t.integer :user_id
+      t.integer :post_id
       t.timestamps null: false
     end
   end
