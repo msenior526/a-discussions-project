0574c303db8d337b6c6d0565fee6bdb388324009
msenior526 <msenior526@gmail.com>
2021-02-12 12:10:58 -0500

    remove name block from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
     has_many :favorites, through: :favorite_recipes, source: :post
     has_secure_password
     validates :username, presence: true, uniqueness: true, length: {in: 6..25}
-    validates :email, presence: true, uniqueness: true, confirmation: true, format: {with:/\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i, message: "please enter a valid email address"}
+    validates :email, presence: true, uniqueness: true, confirmation: true, format: {with:/\A([^@\s]+)@(([-a-z0-9]+)\.([a-z]{2,}))\z/i, message: "please enter a valid email address"}
     validates :password, length: {in: 8..100}, confirmation: true
 end
