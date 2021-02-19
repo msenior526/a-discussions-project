04dd5a7115b6ecba418703f0ce1044dc673e91e9
msenior526 <msenior526@gmail.com>
2021-02-18 20:11:55 -0500

    edit association for favorites

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
     belongs_to :user
     has_many :comments
     has_many :favoriteposts
-    has_many :favorited_by, through: :favorite_recipes, source: :user
+    has_many :favorited_by, through: :favoriteposts, source: :user
     validates :title, :description, presence: true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     has_many :posts
     has_many :comments
     has_many :favoriteposts
-    has_many :favorites, through: :favorite_recipes, source: :post
+    has_many :favorites, through: :favoriteposts, source: :post
     has_secure_password
     validates :username, presence: true, uniqueness: true, length: {in: 6..25}
     validates :email, presence: true, uniqueness: true, confirmation: true, format: {with:/\A([^@\s]+)@(([-a-z0-9]+)\.([a-z]{2,}))\z/i, message: "please enter a valid email address"}
