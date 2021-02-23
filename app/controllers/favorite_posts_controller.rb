34cff5d6d91ad626357d602901a40e52aa030bb3
msenior526 <msenior526@gmail.com>
2021-02-22 23:55:16 -0500

    Cleanup

--- app/controllers/favorite_posts_controller.rb
+++ app/controllers/favorite_posts_controller.rb
@@ -1,14 +1,13 @@
 class FavoritePosts < ApplicationController
 
-    post "/favoriteposts" do
-        post = Post.find_by_id(params[:favoriteposts][:post_id])
-        if !post.favorited_by.include?(current_user)
-          fav = Favoritepost.create(params[:favoriteposts])
-        else
-         favortiepost = Favoritepost.where(["user_id = ? and post_id = ?", current_user.id, post.id]).delete_all
-        #  binding.pry
-        end
-        redirect "/posts/#{post.id}"
+  post "/favoriteposts" do
+    post = Post.find_by_id(params[:favoriteposts][:post_id])
+      if !post.favorited_by.include?(current_user)
+        fav = Favoritepost.create(params[:favoriteposts])
+      else
+        favortiepost = Favoritepost.where(["user_id = ? and post_id = ?", current_user.id, post.id]).delete_all
       end
+    redirect "/posts/#{post.id}"
+  end
 
 end
